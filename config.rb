7d375bbe060e1deb6d647f1de962e64c41ba822b
Sam Richard <snugug@gmail.com>
2013-10-27 11:27:00 -0700

    Updated Compass pipeline directories, turned on relative assets

--- config.rb
+++ config.rb
@@ -2,17 +2,17 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
