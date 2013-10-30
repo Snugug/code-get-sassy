b9794ebaa2675e50bc6cc3ee1c696bfbc8ba0a11
Sam Richard <snugug@gmail.com>
2013-10-30 13:41:52 -0400

    Created JavaScript file to add event listeners to my two buttons and change the href of the stylesheet on click

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+(function() {
+
+  window.onload = function() {
+    var primary = document.getElementById('primary-theme');
+    var secondary = document.getElementById('secondary-theme');
+    var stylesheet = document.getElementsByTagName('link')[0];
+
+    primary.addEventListener('click', function() {
+      stylesheet.href = 'css/style.css';
+    });
+
+    secondary.addEventListener('click', function() {
+      stylesheet.href = 'css/alt.css';
+    });
+  }
+}());
